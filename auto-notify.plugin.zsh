bf73292fd010298b9c12e0d5d6a112dd3a29b539
Michael Aquilina <michaelaquilina@gmail.com>
2019-07-17 21:36:30 +0100

    Improve tracking code and add more tests

--- auto-notify.plugin.zsh
+++ auto-notify.plugin.zsh
@@ -1,11 +1,5 @@
 export AUTO_NOTIFY_VERSION="0.1.0"
 
-# Command that the user has executed
-AUTO_COMMAND=""
-# Full command that the user has executed after alias expansion
-AUTO_COMMAND_FULL=""
-# Command start time in seconds since epoch
-AUTO_COMMAND_START=0
 # Threshold in seconds for when to automatically show a notification
 export AUTO_NOTIFY_THRESHOLD=10
 # List of commands/programs to ignore sending notifications for
@@ -13,6 +7,14 @@ export AUTO_NOTIFY_IGNORE=(
     "vim" "nvim" "less" "more" "man" "tig" "watch" "git commit" "top" "htop" "ssh" "nano"
 )
 
+function _reset_tracking() {
+    # Command start time in seconds since epoch
+    unset AUTO_COMMAND_START
+    # Full command that the user has executed after alias expansion
+    unset AUTO_COMMAND_FULL
+    # Command that the user has executed
+    unset AUTO_COMMAND
+}
 
 function _auto_notify_message() {
     local command="$1"
@@ -51,18 +53,22 @@ function _is_auto_notify_ignored() {
 }
 
 function _auto_notify_send() {
-    local current="$(date +"%s")"
-
-    if [[ "$(_is_auto_notify_ignored "$AUTO_COMMAND_FULL")" == "yes" ]]; then
+    if [[ -z "$AUTO_COMMAND" && -z "$AUTO_COMMAND_START" ]]; then
         return
     fi
 
-    let "elapsed = current - AUTO_COMMAND_START"
+    if [[ "$(_is_auto_notify_ignored "$AUTO_COMMAND_FULL")" == "no" ]]; then
+        local current="$(date +"%s")"
+        let "elapsed = current - AUTO_COMMAND_START"
 
-    if [[ -n "$AUTO_COMMAND" && $elapsed -gt $AUTO_NOTIFY_THRESHOLD ]]; then
-        _auto_notify_message "$AUTO_COMMAND" "$elapsed"
+        if [[ $elapsed -gt $AUTO_NOTIFY_THRESHOLD ]]; then
+            _auto_notify_message "$AUTO_COMMAND" "$elapsed"
+        fi
     fi
-    AUTO_COMMAND=""
+
+    # Empty tracking so that notifications are not
+    # triggered for any commands not run (e.g ctrl+C when typing)
+    _reset_tracking
 }
 
 function _auto_notify_track() {
@@ -83,4 +89,5 @@ function enable_auto_notify() {
 
 autoload -Uz add-zsh-hook
 
+_reset_tracking
 enable_auto_notify
